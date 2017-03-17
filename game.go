2936742b541f7cfb57cb3613a5a148088dbebd96
Michael Alexander <beefsack@gmail.com>
2017-03-17 13:45:49 +1100

    Updated for new game API

--- game.go
+++ game.go
@@ -1,36 +1,15 @@
 package brdgme
 
-import (
-	"io"
-)
-
 // Gamer is a playable game.
-//
-// At a minimum a game only has a log, which works for incredibly simple games.
-// More complex games will want to implement one or more rendering interfaces
-// such as `PlayerTemplater` or `PlayerRenderer`.
 type Gamer interface {
-	Name() string
-	Identifier() string
 	Start(players int) ([]Log, error)
-	AvailableCommands(player int) []CommandDescription
 	Command(
 		player int,
-		input io.Reader,
+		input string,
 		playerNames []string,
-	) (logs []Log, remaining io.Reader, err error)
-	IsFinished() (finished bool, winners []int)
+	) (logs []Log, remaining string, err error)
+	IsFinished() bool
+	Winners() []int
 	WhoseTurn() []int
-}
-
-// A CommandDescription is a description of a command
-type CommandDescription struct {
-	Name        string
-	Description string
-	Example     string
-}
-
-// Eliminator can have eliminated players.
-type Eliminator interface {
-	Eliminated() []int
+	Render(player *int) string
 }
